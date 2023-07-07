6fa7eda29d3997330240833e4f629c3a5b869810
Nishabhooshan <bhooshannisha@gmail.com>
2023-07-08 01:37:08 +0530

    react pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import JsonData from "./JSON_DATA.json";
+import "./App.css";
+import ReactPaginate from "react-paginate";
 
+console.log("nhh", JsonData);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(JsonData.slice(0, 50));
+  const [pageNum, setPageNum] = useState(0);
+
+  const userPerPage = 10;
+  const pagesVisited = pageNum * userPerPage;
+  const showDataPerPage = data
+    .slice(pagesVisited, pagesVisited + userPerPage)
+    .map((user) => {
+      return (
+        <div className="user">
+          <h1>{user.email}</h1>
+          <h2>{user.firstName}</h2>
+          <h3>{user.lastName}</h3>
+        </div>
+      );
+    });
+const pageCount = Math.ceil(data.length/userPerPage)
+const onPageChange = ({selected}) =>{
+  setPageNum(selected)
+}
+  return <div className="App">{showDataPerPage}
+  <ReactPaginate
+  previousLabel ={"Previous"}
+  nextLabel={"Next Label"}
+  pageCount={pageCount}
+  onPageChange={onPageChange}
+  containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+
+  
+
+  />
+  </div>;
 }
 
 export default App;
